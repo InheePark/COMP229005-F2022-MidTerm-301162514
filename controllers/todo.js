3c7a649d9749e1a4d48f4d86ef9020746be6b9de
InheePark <59162676+InheePark@users.noreply.github.com>
2022-10-25 20:31:23 -0400

    todo, routes, views fix

--- controllers/todo.js
+++ controllers/todo.js
@@ -46,6 +46,20 @@ module.exports.details = (req, res, next) => {
 
 // Gets a todo by id and renders the Edit form using the add_edit.ejs template
 module.exports.displayEditPage = (req, res, next) => {
+    let id = req.params.id;
+
+    TodoModel.findById(id, (err, itemToEdit) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.render('todo/add_edit', {
+                title: 'Edit Todo', 
+                todo: itemToEdit
+            })
+        }
+    });
     
     // ADD YOUR CODE HERE
 
@@ -66,21 +80,27 @@ module.exports.processEditPage = (req, res, next) => {
     });
 
     // ADD YOUR CODE HERE
-
-}
-
-// Deletes a todo based on its id.
-module.exports.performDelete = (req, res, next) => {
-
-    // ADD YOUR CODE HERE
-
+    TodoModel.updateOne({_id: id}, updatedTodo, (err) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.redirect('/todo/list');
+        }
+    })
 }
 
 // Renders the Add form using the add_edit.ejs template
 module.exports.displayAddPage = (req, res, next) => {
 
     // ADD YOUR CODE HERE          
+    let newItem = TodoModel();
 
+    res.render('todo/add_edit', {
+        title: 'Add a new Todo', 
+        todo: newItem
+    })
 }
 
 // Processes the data submitted from the Add form to create a new todo
@@ -96,5 +116,32 @@ module.exports.processAddPage = (req, res, next) => {
     });
 
     // ADD YOUR CODE HERE
+    TodoModel.create(newTodo, (err, item) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            console.log(item);
+            res.redirect('/todo/list');
+        }
+    })
     
+}
+
+// Deletes a todo based on its id.
+module.exports.performDelete = (req, res, next) => {
+    let id = req.params.id;
+    // ADD YOUR CODE HERE
+
+    TodoModel.remove({_id: id}, (err) => {
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.redirect('/todo/list');
+        }
+    })
+
 }
